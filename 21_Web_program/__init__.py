95d0b4431f0fd679264e0d0e54c83a10eb437580
kHRYSTAL <723526676@qq.com>
2016-05-27 00:21:54 +0800

    update

---
+++ 21_Web_program/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+"""
+@version: ??
+@usage: 
+@author: kHRYSTAL
+@license: Apache Licence 
+@contact: khrystal0918@gmail.com
+@site: https://github.com/kHRYSTAL
+@software: PyCharm
+@file: __init__.py.py
+@time: 16/5/27 上午12:20
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
