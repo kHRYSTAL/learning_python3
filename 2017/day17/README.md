f083a2426128bdd68b1819e5a56c6613d654aa46
kHRYSTAL <723526676@qq.com>
2017-09-07 00:18:13 +0800

    update day17

--- 2017/day17/README.md
+++ 2017/day17/README.md
@@ -4,6 +4,34 @@ POST 提交数据
 ##### 路由系统URL
 
 ##### 视图 View
+    1. 请求对象
+        request.POST    表单提交都从POST中接收
+        request.GET     url提交都从GET接收
+        request.FILES   文件提交都从FILE接收 html的form要加enctype="multipart/form-data"
+
+    2. 获取请求数据
+        1.获取单个数据:
+            single_val = request.POST.get('gender')
+        2.获取相同name的多个数据:
+            multi_val = request.POST.getlist('favour')
+
+        3.获取上传的文件
+            file_obj = request.FILES.get('upload_file')
+            if file_obj is not None:
+                print('获取上传文件', file_obj, type(file_obj))
+                print('文件名称', file_obj.name)
+                # 获取上传的文件 保存在服务器
+                import os
+                file_path = os.path.join('upload', file_obj.name)
+                f = open(file_path, mode='wb')
+                # chunks 是上传文件的块 是一个生成器 需要迭代
+                for block in file_obj.chunks():
+                    f.write(block)
+                f.close()
+            else:
+                print('文件为空')
+
+
 
 ##### 模板 Template
 
--- 2017/day17/my_django_project/app01/views.py
+++ 2017/day17/my_django_project/app01/views.py
@@ -8,17 +8,54 @@ def index(request):
     return HttpResponse('index')
 
 
+# def login(request):
+#     if request.method == 'GET':
+#         return render(request, 'login.html')
+#     elif request.method == 'POST':
+#         u = request.POST.get('user')
+#         p = request.POST.get('pwd')
+#         if u == 'matt' and p == '123':  # 用户名密码正确, 返回首页
+#             return redirect('/index/')
+#         else:  # 不正确render当前页面
+#             return render(request, 'login.html')
+#     else:
+#         # PUT DELETE HEAD OPTION
+#         # 其他方式提交 跳转回首页
+#         return redirect('/index/')
+
 def login(request):
     if request.method == 'GET':
         return render(request, 'login.html')
     elif request.method == 'POST':
-        u = request.POST.get('user')
-        p = request.POST.get('pwd')
-        if u == 'matt' and p == '123':  # 用户名密码正确, 返回首页
-            return redirect('/index/')
-        else:  # 不正确render当前页面
-            return render(request, 'login.html')
+        single_val = request.POST.get('gender')
+        print("获取单选gender", single_val)
+        multi_val = request.POST.getlist('favour')
+        print("获取多选favour", multi_val)
+
+        multi_city_val = request.POST.getlist('city')
+        print("获取多选city", multi_city_val)
+
+        file_obj = request.FILES.get('upload_file')
+        if file_obj is not None:
+            print('获取上传文件', file_obj, type(file_obj))
+            print('文件名称', file_obj.name)
+
+            # 获取上传的文件 保存在服务器
+            import os
+            file_path = os.path.join('upload', file_obj.name)
+            f = open(file_path, mode='wb')
+            # chunks 是上传文件的块 是一个生成器 需要迭代
+            for block in file_obj.chunks():
+                f.write(block)
+            f.close()
+        else:
+            print('文件为空')
+
+        return render(request, 'login.html')
     else:
         # PUT DELETE HEAD OPTION
         # 其他方式提交 跳转回首页
         return redirect('/index/')
+
+
+
