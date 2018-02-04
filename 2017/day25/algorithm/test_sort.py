4e735fe9613a63123f73de47dd479bcb8c5ca8fd
kHRYSTAL <723526676@qq.com>
2018-02-04 21:45:40 +0800

    add solution q1

--- 2017/day25/algorithm/test_sort.py
+++ 2017/day25/algorithm/test_sort.py
@@ -19,6 +19,31 @@
 """
 
 
+def binary_search(data_set, value):
+    """get index by value, condition is data_set is a sort list"""
+    low = 0
+    high = len(data_set) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if data_set[mid] == value:
+            # return mid
+            left = mid
+            right = mid
+            while left >= 0 and data_set[left] == value:
+                left -= 1
+            while right < len(data_set) and data_set[right] == value:
+                right += 1
+            return left + 1, right - 1
+        elif data_set[mid] < value:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return
+
+
+li = [1, 2, 3, 3, 3, 4, 4, 5]
+# TODO get value 3 index scope (2, 4)
+print(binary_search(li, 5))
 """
 给定一个升序列表和一个整数, 返回该整数在列表中的下标范围
 例如 列表[1, 2, 3, 3, 3, 4, 4, 5] 若查找3 则返回(2, 4)
