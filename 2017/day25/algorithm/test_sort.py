67661efae8e651087237d59e023d5e8c1f803883
kHRYSTAL <723526676@qq.com>
2018-02-05 23:55:57 +0800

    add solution q2

--- 2017/day25/algorithm/test_sort.py
+++ 2017/day25/algorithm/test_sort.py
@@ -17,10 +17,58 @@
 
 例如 列表[1, 2, 5, 4] 与目标整数3, 1+2=3, 结果为(0, 1)
 """
+li = [1, 2, 5, 4]
+target = 3
 
 
-def binary_search(data_set, value):
-    """get index by value, condition is data_set is a sort list"""
+def func1():
+    for i in range(len(li)):
+        if li[i] > target:
+            # 都为正数情况下 sum 值一定比两个数大
+            continue
+        for j in range(i + 1, len(li)):
+            if li[i] + li[j] == target:
+                return i, j
+
+
+print(func1())
+
+
+def binary_search(data_set, value, low, high):
+    while low <= high:
+        mid = (low + high) // 2
+        if data_set[mid] == value:
+            return mid
+        elif data_set[mid] > value:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return
+
+
+def func2():
+    import copy
+    temp_set = copy.deepcopy(li)  # 深复制列表用于排序后通过二分查找快速找到第二个数
+    temp_set.sort()
+    for i in range(len(temp_set)):
+        a = i  # 第一个数的索引
+        b = binary_search(temp_set, target - temp_set[a], i + 1, len(temp_set) - 1)  # 通过二分查找找到第二个数
+        if b:  # 如果第二个数存在
+            return li.index(temp_set[a]), li.index(temp_set[b])  # 返回两个数在原列表的索引
+
+print(func2())
+"""
+给定一个升序列表和一个整数, 返回该整数在列表中的下标范围
+例如 列表[1, 2, 3, 3, 3, 4, 4, 5] 若查找3 则返回(2, 4)
+若查找1 则返回(0, 0)
+"""
+
+
+def get_value_scope(data_set, value):
+    """
+    get index by value, condition is data_set is a sort list
+    it is use binary search
+    """
     low = 0
     high = len(data_set) - 1
     while low <= high:
@@ -43,9 +91,4 @@ def binary_search(data_set, value):
 
 li = [1, 2, 3, 3, 3, 4, 4, 5]
 # TODO get value 3 index scope (2, 4)
-print(binary_search(li, 5))
-"""
-给定一个升序列表和一个整数, 返回该整数在列表中的下标范围
-例如 列表[1, 2, 3, 3, 3, 4, 4, 5] 若查找3 则返回(2, 4)
-若查找1 则返回(0, 0)
-"""
+# print(get_value_scope(li, 5))
