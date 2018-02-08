bb6d8601500ea9656a1f73b20bcf60d775926d1d
kHRYSTAL <723526676@qq.com>
2018-02-08 11:12:42 +0800

    add lucky draw orm

--- 2017/something/lucky_draw/lucky/models.py
+++ 2017/something/lucky_draw/lucky/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class UserInfo(models.Model):
+    username = models.CharField(max_length=32)
+
+
+class Gift(models.Model):
+    name = models.CharField(max_length=32)
--- 2017/something/lucky_draw/lucky/views.py
+++ 2017/something/lucky_draw/lucky/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def users(request):
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        pass
